3b4787235c2c958a143e84817cc0591bf37996dc
sophie-davis <143359039+sophie-davis@users.noreply.github.com>
2023-09-15 12:34:58 -0400

    cleaned up hw1

---
+++ pui-lab/hello_world.js
@@ -0,0 +1,45 @@
+
+let assisgmentScores = [30, 20, 50];
+
+//totalScore = assisgmentScores[0] + assisgmentScores[1] + assisgmentScores[2];
+
+//console.log(totalScore);
+
+function calculateTotal(hw1, hw2, hw3) {
+    let total = hw1 + hw2 + hw3;
+    return (total);
+}
+
+let totalScore = calculateTotal(assisgmentScores[0], assisgmentScores[1], assisgmentScores[2]);
+console.log(totalScore);
+
+/*
+let grade;
+
+if (totalScore >= 92) {
+    grade = "A";
+}
+else if (85 <= totalScore && totalScore < 92) {
+    grade = "B";
+} else {
+    grade = "C";
+}
+
+console.log(grade);
+
+
+
+for (let i=0; i<20;i++) {
+    console.log("meow")
+}
+
+let i =0;
+let text = "";
+
+while (i<10) {
+    text += "The number is" + i;
+    console.log(text);
+    i++;
+}
+
+*/
